2fbafe907bf67686a4df26b3181b67613edb61e2
SabiMantock <sabimantock@gmail.com>
2023-01-21 16:26:41 +0000

    fixed issues

--- src/tests/count.test.js
+++ src/tests/count.test.js
@@ -4,8 +4,10 @@
 
 import counter from '../modules/counter.js';
 
-test('should first', async () => {
+test('expect length of items to be 2', async () => {
   const items = ['firstItem', 'secondItem'];
+  const emptyCount = counter([]);
   const count = counter(items);
+  expect(emptyCount).toBe(0);
   expect(count).toBe(2);
 });
